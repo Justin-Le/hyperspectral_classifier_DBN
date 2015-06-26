6f7bff0ae3c76743afa08090becf5506ffbaa925
Justin Le <lejustin.lv@gmail.com>
2015-06-25 19:15:44 -0700

    Loads model and classifies Pavia Centre image

--- DBN_example.py
+++ DBN_example.py
@@ -10,10 +10,10 @@ import theano.tensor as T
 from logistic_sgd import LogisticRegression, load_data
 from mlp import HiddenLayer
 from rbm import RBM
-from DBN import DBN
+from DBN_writeparams import DBN
 
-def test_DBN_example(dataset='mnist.pkl.gz', batch_size=10):
-    """"
+def test_DBN_example(dataset='none', batch_size=10):
+    """
     Tests a trained DBN on a set of images.
 
     For each batch:
@@ -25,17 +25,56 @@ def test_DBN_example(dataset='mnist.pkl.gz', batch_size=10):
     :type dataset: string
     :param dataset: path to the pickled data
     :type batch_size: int
-    :param batch_size: the size of a minibatch
-    """"
+    :param batch_size: the size of a minibatch    
+    """
 
-    datasets = load_data(dataset)
+    if dataset == 'none':
+	print '\nLoading the image to be classified . . .\n'
 
-    example_set_x, example_set_y = datasets[0]
+	# Load images and ground truth from
+	# text files that are delimited by whitespace
+	imraw = numpy.loadtxt('pavia_centre_image_100rows1.txt')
+	gtraw = numpy.loadtxt('pavia_centre_groundtruth.txt')
+
+	val_idx = 0
+	example_size = 102
+	imlist = []
+
+	# imlist is a list of images,
+	# where each image is a list of values 
+	while val_idx < len(imraw):
+	  imlist.append(imraw[val_idx : val_idx + example_size])
+	  val_idx = val_idx + example_size
+
+	# Store images and ground truths as numpy arrays in shared variables
+	# in order to use them in Theano
+        borrow = True
+        example_set_x = theano.shared(numpy.asarray(imlist,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        example_set_y = T.cast(
+                                  theano.shared(numpy.asarray(gtraw,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow),
+                                  'int32'
+                              )
+
+        # store as a shared variable in order to print dimensions
+	example_set_y_shared = theano.shared(numpy.asarray(gtraw))
+
+        print (('\nDimensions of image set (pixels by bands-per-pixel): %s\n') % ((example_set_x.get_value().shape,)))
+        print (('\nDimensions of ground truth set (number of pixel labels): %s\n') % ((example_set_y_shared.get_value().shape,)))
+
+    else:
+	datasets = load_data(dataset)
+
+	example_set_x, example_set_y = datasets[0]
+
 
     n_batches = example_set_x.get_value(borrow=True).shape[0] / batch_size 
+    print(('\nnumber of batches: %i\n') % (n_batches))
 
-    print(('number of batches: %i') % (n_batches))
-
+    # Create the DBN object
     numpy_rng = numpy.random.RandomState(123)
     n_ins = 28*28
     hidden_layers_sizes = [10, 10, 10]
@@ -45,7 +84,7 @@ def test_DBN_example(dataset='mnist.pkl.gz', batch_size=10):
               hidden_layers_sizes=hidden_layers_sizes,
               n_outs=n_outs)
 
-    print 'Loading model . . . '
+    print '\nLoading weights and biases . . .\n'
     
     # If the text file is a list of weights,
     # delimited by whitespace,
@@ -54,9 +93,7 @@ def test_DBN_example(dataset='mnist.pkl.gz', batch_size=10):
     loaded_weights[0] = numpy.loadtxt('weights_layer0.txt')
     loaded_weights[1] = numpy.loadtxt('weights_layer1.txt')
     loaded_weights[2] = numpy.loadtxt('weights_layer2.txt')
-
-    # print loaded_weights
-
+    
     # As above, loaded_biases[i] is a numpy array of floats
     loaded_biases = [numpy.asarray([0])] * 3
     loaded_biases[0] = numpy.loadtxt('biases_layer0.txt')
@@ -69,16 +106,20 @@ def test_DBN_example(dataset='mnist.pkl.gz', batch_size=10):
         dbn.sigmoid_layers[layer_index].W.set_value(loaded_weights[layer_index])
         dbn.sigmoid_layers[layer_index].b.set_value(loaded_biases[layer_index])
 
-        print (('Dimensions of layer %i weights: ') % (layer_index))
+        print (('\nDimensions of layer %i weights:\n') % (layer_index))
         print loaded_weights[layer_index].shape
-        print (('Dimensions of layer %i biases: ') % (layer_index))
+        print '\n'
+        print (('\nDimensions of layer %i biases:\n') % (layer_index))
         print loaded_biases[layer_index].shape
+        print '\n'
 
-        print (('Layer %i weights: ') % (layer_index)) 
+        print (('\nLayer %i weights:\n') % (layer_index)) 
         print loaded_weights[layer_index]
-        print (('Layer %i biases: ') % (layer_index)) 
+        print '\n'
+        print (('\nLayer %i biases:\n') % (layer_index)) 
         print loaded_biases[layer_index]
-       
+        print '\n' 
+
     # start-snippet-2
      
     # example index
@@ -112,7 +153,12 @@ def test_DBN_example(dataset='mnist.pkl.gz', batch_size=10):
                     #       test_score * 100.))
         
                     if batch_index%1000 == 0:
-  		        print(y_prob, y_pred)
+  		        print '\nProbabilities of y given x:\n'
+                        print y_prob 
+                        print '\n'
+  		        print '\nPredicted classes y:\n'
+                        print y_pred
+                        print '\n'
 
     end_time = time.clock()
     print(
